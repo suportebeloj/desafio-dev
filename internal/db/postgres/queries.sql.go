b41d8bca75715dd877afbc1d9b72d481fa354ac9
Carlos Eduardo <suportebeloj@gmail.com>
2023-05-14 16:45:20 -0300

    refactor(db): :hammer: group stores by name

--- internal/db/postgres/queries.sql.go
+++ internal/db/postgres/queries.sql.go
@@ -154,6 +154,7 @@ func (q *Queries) ListMarketTransaction(ctx context.Context, market string) ([]L
 const listMarkets = `-- name: ListMarkets :many
 SELECT market
 FROM desafio_dev.public.transactions
+GROUP BY market
 `
 
 func (q *Queries) ListMarkets(ctx context.Context) ([]string, error) {
@@ -181,11 +182,11 @@ func (q *Queries) ListMarkets(ctx context.Context) ([]string, error) {
 
 const marketBalance = `-- name: MarketBalance :one
 SELECT CAST(sum(
-               CASE
-                   WHEN type IN ('1', '4', '5', '6', '7', '8') THEN value
-                   WHEN type IN ('2', '3', '9') THEN -value
-                   END
-           ) AS FLOAT) AS total_balance
+        CASE
+            WHEN type IN ('1', '4', '5', '6', '7', '8') THEN value
+            WHEN type IN ('2', '3', '9') THEN -value
+            END
+    ) AS FLOAT) AS total_balance
 FROM desafio_dev.public.transactions
 WHERE market = $1
 `
--- sqlc/queries/queries.sql
+++ sqlc/queries/queries.sql
@@ -10,17 +10,18 @@ WHERE market = $1;
 
 -- name: MarketBalance :one
 SELECT CAST(sum(
-               CASE
-                   WHEN type IN ('1', '4', '5', '6', '7', '8') THEN value
-                   WHEN type IN ('2', '3', '9') THEN -value
-                   END
-           ) AS FLOAT) AS total_balance
+        CASE
+            WHEN type IN ('1', '4', '5', '6', '7', '8') THEN value
+            WHEN type IN ('2', '3', '9') THEN -value
+            END
+    ) AS FLOAT) AS total_balance
 FROM desafio_dev.public.transactions
 WHERE market = $1;
 
 -- name: ListMarkets :many
 SELECT market
-FROM desafio_dev.public.transactions;
+FROM desafio_dev.public.transactions
+GROUP BY market;
 
 -- name: CreateTransaction :one
 INSERT INTO desafio_dev.public.transactions (type, date, value, cpf, card, time, owner, market)
