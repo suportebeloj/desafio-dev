46d2d357f97d65e190a6b9ea37f41bf68e78785e
Carlos Eduardo <suportebeloj@gmail.com>
2023-05-14 11:14:03 -0300

    feat(cmd): :sparkles: create dependencies and running http service

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/suportebeloj/desafio-dev/internal/cmd/settings"
+	"github.com/suportebeloj/desafio-dev/internal/core"
+	"github.com/suportebeloj/desafio-dev/internal/db/postgres"
+	"github.com/suportebeloj/desafio-dev/internal/frameworks/http/api"
+	"github.com/suportebeloj/desafio-dev/internal/usecases"
+	"log"
+)
 
 func main() {
-	fmt.Println("Desafio-dev")
+	dbService := postgres.New(settings.DbConn.Conn)
+	parser := core.NewTransactionParser()
+	transactionService := usecases.NewTransactionService(dbService, parser)
+
+	log.Println("transaction service running", transactionService)
+
+	httpService := api.NewHTTPApiSerice(transactionService)
+	_ = httpService.Run(":8000")
+
 }
